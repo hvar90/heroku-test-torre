f382c55ab5931a3a9648c510cdaaaaa5473695c4
Heberth vargas Tello <hvar90@gmail.com>
2016-10-07 17:30:20 -0500

    Update urls.py

--- heberthVargasTestTorre/apiV1/urls.py
+++ heberthVargasTestTorre/apiV1/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url, include
 
 
-#urlpatterns = patterns("", url(r"^$", "api"),
-  
-    ##url(r"^", include("api1.message.urls")),
- 
-#)
+
 urlpatterns = [
      url(r"^", include("apiV1.test.urls")), 
 ]
