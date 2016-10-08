7dfe0ecfabfe197a3d1d4652136fedcd2a63f215
hvar90 <hvar90@gmail.com>
2016-10-07 21:06:24 -0500

    cambio de nombre carpeta test

---
+++ heberthVargasTestTorre/apiV1/palindrome/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls           import url
+
+from .views import *
+
+urlpatterns = [ 	      
+    url(r"^palindrome/(?P<x>\d+)/(?P<y>\d+)/$",
+        PalindromeView.as_view(),prefix='api1.palindrome.views'),
+]
+
+
