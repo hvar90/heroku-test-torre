ba8a5f55565d2fcf852fd5e715214e8b0df1cc0b
hvar90 <hvar90@gmail.com>
2016-10-09 23:58:35 -0500

    mejora velocidad de algoritmo

--- heberthVargasTestTorre/palindrome/models.py
+++ heberthVargasTestTorre/palindrome/models.py
@@ -1,33 +1,20 @@
-
-
 class PalindromeBinary(object) :	
-	def get_palindrome_list(self,x,y):
-		global num_cycles 
-		num_cycles=0
+	def get_palindrome_list(self,x,y):		
 		is_palindrome=self._is_palindrome
 		result=[num for num in xrange(x,y)
 				  if is_palindrome(bin(num)[2:])]
-		complexity="{0}*O({1})".format(
-								(y-x), len(str(bin((y))[2:])))	
-		"""la complejidad en el peor de los casos esta dada por el
-		tamano del mayor numero en binario
-		osea len((bin(y)) esto se multiplica por la cantidad de
-		numeros totales a analizar osea (y-x)"""	  
+		complexity="O({0})".format(y-x)	
+		"""la complejidad  esta dada  por la cantidad de
+		numeros totales a analizar osea (y-x)
+		osea tiene una complejidad lineal O(y-x)"""	  
 		return {"total_palindromes":len(result),
 				"list_palindrome":result,
-				"total_cycles":num_cycles+y-x,
-				"complexity": complexity}
+				"total_cycles":y-x,
+				"complexity": complexity}	
 		
+	def _is_palindrome(self,word):						
+			return word[::-1]==word
 		
-		
-	def _is_palindrome(self,word):
-		global num_cycles
-		inverseWord = word[::-1]
-		for index,letter in enumerate(word) :
-			num_cycles+=1
-			if inverseWord[index] != letter:
-				return False 
-		return True
 		
 		
 		
