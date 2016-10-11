ac2f6e024d53b5c517cdf2adbade1858671b4115
hvar90 <hvar90@gmail.com>
2016-10-11 01:43:41 -0500

    arreglo complejidad algoritmo

--- heberthVargasTestTorre/palindrome/models.py
+++ heberthVargasTestTorre/palindrome/models.py
@@ -1,27 +1,21 @@
+"""la complejidad  esta dada  por la cantidad de
+	numeros palindromes que hay contando desde el numero 1
+	hasta el numero superior del rango dado			
+	la complejidad puede ser vista de orden logaritmico
+	si nuestro n=y-x donde X y Y son nuestros limites inferior y superior 
+	en el rango de numeros para obtener numeros palindromos
+	entonces la complejidad es O(log (y-x))	
+	por ultimo si npal= numero total de palindromos en nuestro rango
+	entonces a esto se le suma los ciclos de nuestros 2 ciclos While
+	cada ciclo while es aproximadamente igual a la mitad 
+	del total de numeros palindromos npal/2, por ser valores
+	pequenos no afectan mucho a la complejidad del programa"""		  
 class PalindromeBinary(object) :	
 	
 	def get_palindrome_list(self,x,y):		
 		self.num_cycles=0
 		result=list(self._make_gen(x,y))	
-		complexity="O({0})".format(self.num_cycles)
-		"""la complejidad  esta dada  por la cantidad de
-		numeros palindromes que hay contando desde el numero 1
-		hasta el numero superior del rango dado
-		la formula para hallar la cantidad de numeros palindromes desde 
-		1 hasta nuestro limite de nuestro rango superior es  3·2(n-1)/2 – 2 ,
-		ver : http://www.exploringbinary.com/counting-binary-and-hexadecimal-palindromes
-		y n seria la cantidad de digitos binarios que tenga nuestro 
-		limite superior de nuestro rango es decir len(bin(y))
-		esto haria que la complejidad sea de orden exponencial respecto
-		a la cantidad de digitos binarios de nuestro limite superior del rango		 
-		osea tiene una complejidad exponencial O(a**n))
-		donde a es 2 y n=len(bin(y))
-		por ultimo si npal= numero total de palindromos en nuestro rango
-		entonces a esto se le suma los ciclos de nuestros 2 ciclos While
-		cada ciclo while es aproximadamente igual a la mitad 
-		del total de numeros palindromos npal/2
-		la complejidad tambien puede seri vista de orden logaritmico
-		si nuestro n=y-x entonces la complejidad es O(log (y-x))"""		  
+		complexity="O({0})".format(self.num_cycles)		
 		return {"total_palindromes":len(result),
 				"list_palindrome":result,
 				"total_cycles":self.num_cycles,
