3fa220cdfaf9fffcf226a4a7c54ef6777b798dd8
hvar90 <hvar90@gmail.com>
2016-10-10 21:29:45 -0500

    mejora velocidad de algoritmo con formula matematica y uso de generadores

--- heberthVargasTestTorre/palindrome/models.py
+++ heberthVargasTestTorre/palindrome/models.py
@@ -1,21 +1,46 @@
 class PalindromeBinary(object) :	
 	
 	def get_palindrome_list(self,x,y):		
-		is_palindrome=self._is_palindrome
-		result=[num for num in xrange(x,y)
-				  if is_palindrome(bin(num)[2:])]
-		complexity="O({0})".format(y-x)	
+		self.num_cycles=0
+		result=list(self._make_gen(x,y))	
+		complexity="O({0})".format(self.num_cycles)
 		"""la complejidad  esta dada  por la cantidad de
-		numeros totales a analizar osea (y-x)
-		osea tiene una complejidad lineal O(y-x)"""	  
+		numeros numeros palindros que hay contando desde el numero cero
+		hasta el numero superior del rango dado
+		osea tiene una complejidad lineal O(n)"""		  
 		return {"total_palindromes":len(result),
 				"list_palindrome":result,
-				"total_cycles":y-x,
+				"total_cycles":self.num_cycles,
 				"complexity": complexity}	
 		
-	def _is_palindrome(self,word):						
-		return word[::-1]==word
-		
+		def _make_gen(self,start,end):
+			gen=self._palgen_base2()	
+			while True:
+				pal=next(gen)			
+				if start<=pal<=end:
+					yield pal
+				elif pal>end:
+					 break   
+			
+	def _palgen_base2(self): 
+		"""este algoritmo esta creado basandose en la formula matematica
+		para sacar secuencias de numeros binarios que sean palindromes
+		ver: https://oeis.org/A006995"""  
+		yield 0
+		x, n, n2 = 1, 1, 2		
+		while True:
+			for y in range(n, n2):
+				s = format(y, 'b')
+				self.num_cycles+=1			
+				yield int(s+s[-2::-1], 2)
+			for y in range(n, n2):
+				s = format(y, 'b')
+				self.num_cycles+=1
+				
+				yield int(s+s[::-1], 2)
+			x += 1
+			n *= 2
+			n2 *= 2
 		
 		
 		
